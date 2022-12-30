37d59da3f05091fe87d6cec00cce7bffc93a197d
Prajuk Nusbaum <deomax@yahoo.com>
2022-12-30 10:31:44 -0600

    done

--- NullVariables/Program.cs
+++ NullVariables/Program.cs
@@ -4,6 +4,11 @@
 // haven't asked for age just yet
 //has the capacity to set a null ?
 int? age = null;
+bool? birthday = null;
+double? battingAverage= null;
+
+string? firstName = null;
+
 
 // we have asked for age now
 age = 0;
